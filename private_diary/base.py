9d19d49709b706cc1c0023937f93394ba0f97353
Hiroto <hiroto.fukada@gmail.com>
2023-08-25 15:53:03 +0900

    migrationsができないからcommitしてみた

--- private_diary/base.py
+++ private_diary/base.py
@@ -35,8 +35,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "diary",
-    "accounts",
+    "diary.apps.DiaryConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
